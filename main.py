38119546b363dd3c732789eb15e71f46c64acc27
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2020-06-05 17:17:10 +0900

    maattermost

--- main.py
+++ main.py
@@ -2,6 +2,34 @@
 import sys
 import re
 
-data = sys.stdin.read()
-if not data.endswith("\n"):
-    data += "\n"
+MATTERMOST_IGNORES = """
+q""".strip().split("\n")
+
+
+def twitter_to_scrapbox(data):
+    result = ""
+    data = re.sub(r".* profile image	", "", data)
+    data = re.sub(r"午(前|後)\d+:\d+.*\n", "", data)
+    data = re.sub(r"nisbot\nBOT\n", "[nisbot.icon]", data)
+    data = re.sub(r"\nnishio\n", "\n[nishio.icon]", data)
+    data = re.sub(r"\n今日\n", "\n", data)
+
+    for line in data.split("\n"):
+        line = line.strip()
+        if not line:
+            continue
+        if line in MATTERMOST_IGNORES:
+            continue
+        result += line + "\n"
+    return result
+
+
+def main():
+    data = sys.stdin.read()
+    if not data.endswith("\n"):
+        data += "\n"
+    print(twitter_to_scrapbox(data))
+
+
+if __name__ == "__main__":
+    main()
